cf6255058376890eac62f7fc1031f2f6158570b1
Steve Gravrock <sgravrock@pivotal.io>
2016-02-11 09:49:24 -0800

    Basic test gear
    Signed-off-by: Steve Gravrock <sgravrock@pivotal.io>
    Signed-off-by: Augustus Lidaka <alidaka@pivotal.io>

---
+++ README.md
@@ -0,0 +1,8 @@
+== Setup ==
+
+1. `bundle install`
+
+
+== Usage ==
+
+To run the tests: `bundle exec rake spec`
---
+++ spec/ServiceTester_spec.rb
@@ -0,0 +1,5 @@
+describe "ServiceTester" do
+  it "should do some stuff" do
+    expect(true).to eq(true)
+  end
+end
